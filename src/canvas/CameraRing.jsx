ae76814300ef5532ef33440d0b9fbf8cf91e5156
Hirwa-Joric <122998728+Hirwa-Joric@users.noreply.github.com>
2023-05-03 19:55:58 +0200

    intit commit

---
+++ src/canvas/CameraRing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CameraRing = () => {
+  return (
+    <div>CameraRing</div>
+  )
+}
+
+export default CameraRing
---
+++ src/canvas/index.jsx
@@ -0,0 +1,14 @@
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from "@react-three/drei"
+
+
+import Shirt from './Shirt'
+import Backdrop from './Backdrop'
+import CameraRing from './CameraRing'
+const CanvasModel = () => {
+  return (
+    <div>CanvasModel</div>
+  )
+}
+
+export default CanvasModel
